23f4a86f73c9eac707dcc5d21bca48fec2c18413
Michael Green <mgreen1980@gmail.com>
2019-01-19 23:02:27 +0000

    actually getting data now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    use std::io::Read;
+
+    let mut resp = reqwest::get("https://store.steampowered.com/api/appdetails/?appids=391220")?;
+    assert!(resp.status().is_success());
+
+    let mut content = String::new();
+
+    resp.read_to_string(&mut content).expect("could not read file");
+    
+    println!("{:#?}", content);
+    Ok(())
 }
