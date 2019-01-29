b3d99f60602b1e9130fd98aa9ba85e5c69362f33
Michael Green <mgreen1980@gmail.com>
2019-01-29 00:47:02 +0000

    works on multiple appids, no error handling yet

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,34 @@ extern crate reqwest;
 extern crate serde_json;
 
 fn main() {
-    get_stuff("8930".to_string());
+    let mut ids: Vec<String> = std::env::args().collect();
+
+    if ids.len() == 1 {
+        // TODO: parse steam library directories from compatdata
+        // should use default directory or env variable if it is set
+        // if env variable not set and nothing is found, print msg about setting it
+    }
+    else {
+        ids.swap_remove(0);
+    }
+
+    for id in ids.iter() {
+        let response = query_appid(id);
+
+        let json: serde_json::Value = serde_json::from_str(&response)
+            .expect("JSON was not well-formatted");
+
+        let name = json.get(id).unwrap().get("data").unwrap().get("name").unwrap().as_str().unwrap();
+        
+        println!("{}", name);
+    }
 }
 
-fn get_stuff(appid: String) {
+fn query_appid(appid: &String) -> String {
     let url = format!("https://store.steampowered.com/api/appdetails/?appids={}", appid);
-    
-    match reqwest::get(&url) {
-        Ok(mut response) => {
-            if response.status() == reqwest::StatusCode::OK {
-                match response.text() {
-                    Ok(text) => println!("{}", text),
-                    Err(_) => println!("Unable to read response text")
-                }
-            }
-            else {
-                println!("Response NOT 200 Ok");
-            }
-        }
-        Err(_) => println!("Error: Did not get a response")
-    }
+
+    reqwest::get(&url)
+        .expect(&format!("Unable to get response for {}", appid))
+        .text()
+            .expect("Unable to parse response")
 }
